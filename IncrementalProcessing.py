d7f96b527fd216d7a82af5a72c8400b3da03ad88
kartheekbezawada <kartheekbezawada@outlook.com>
2023-12-26 16:08:54 +0000

    Update IncrementalProcessing.py

--- IncrementalProcessing.py
+++ IncrementalProcessing.py
@@ -1,57 +1,71 @@
-from pyspark.sql import SparkSession, Row
-from datetime import datetime
+from pyspark.sql import SparkSession
 from azure.identity import DefaultAzureCredential
-from azure.keyvault.secrets import SecretClient
 from azure.storage.blob import BlobServiceClient
-import traceback
-import os
-from dbuutils import DBUtils
 
 class DataMigrator:
     def __init__(self):
         self.spark = SparkSession.builder.appName("DataMigration").getOrCreate()
-        self.key_vault_scope = "key-vault-scope"
-        self.alpha_storage_account = self.get_secret(scope = "key-vault-scope", key = "alpha-storage-account-name")
-        self.alpha_container = self.get_secret(scope = "key-vault-scope", key = "alpha-container-name")
-        self.beta_storage_account = self.get_secret(scope = "key-vault-scope", key = "beta-storage-account-name")
-        self.beta_container = self.get_secret(scope = "key-vault-scope", key = "beta-storage-container-name")
-        self.jdbc_hostname = self.get_secrets(scope = "key-vault-scope", key = "sql-server-hostname")
-        self.jdbc_database = self.get_secrets(scope = "key-vault-scope", key = "sql-database-name")
-        self.jdbc_username = self.get_secrets(scope = "key-vault-scope", key = "sql-username")
-        self.jdbc_password = self.get_secrets(scope = "key-vault-scope", key = "sql-password")
-        
-    # Mount storage containers, if not already mounted, if mounted print message
-    def mount_storage_containers(self):
+        self.alpha_container = "alpha-container-name"
+        self.beta_container = "beta-container-name"
+        self.alpha_storage_account = "alpha-storage-account-name"
+        self.beta_storage_account = "beta-storage-account-name"
+        self.mount_points = [f"/mnt/{self.alpha_container}", f"/mnt/{self.beta_container}"]
+        self.connect_to_azure_storage()
+
+    def connect_to_azure_storage(self):
         try:
-            dbutils.fs.ls(f"/mnt/{self.alpha_container}")
-            print(f"/mnt/{self.alpha_container} is already mounted")
-        except Exception as e:
-            print(f"Mounting /mnt/{self.alpha_container}")
-            dbutils.fs.mount(
-                source = f"wasbs://{self.alpha_container}@{self.alpha_storage_account}",
-                mount_point = f"/mnt/{self.alpha_container}",
-                extra_configs = self.get_storage_account_config(self.alpha_storage_account)
+            credential = DefaultAzureCredential()
+            self.blob_service_client_alpha = BlobServiceClient(
+                account_url=f"https://{self.alpha_storage_account}.blob.core.windows.net",
+                credential=credential
             )
+            self.blob_service_client_beta = BlobServiceClient(
+                account_url=f"https://{self.beta_storage_account}.blob.core.windows.net",
+                credential=credential
+            )
+        except Exception as e:
+            print(f"Error connecting to Azure Storage: {str(e)}")
+            raise e
+
+    def list_folders(self, container_name, prefix=None):
         try:
-            dbutils.fs.ls(f"/mnt/{self.beta_container}")
-            print(f"/mnt/{self.beta_container} is already mounted")
-        except Exception as e:
-            print(f"Mounting /mnt/{self.beta_container}")
-            dbutils.fs.mount(
-                source = f"wasbs://{self.beta_container}@{self.beta_storage_account}",
-                mount_point = f"/mnt/{self.beta_container}",
-                extra_configs = self.get_storage_account_config(self.beta_storage_account)
+            container_client = (
+                self.blob_service_client_alpha.get_container_client(container_name)
+                if container_name == self.alpha_container
+                else self.blob_service_client_beta.get_container_client(container_name)
             )
-    # List all folders in a root folder of a storage container and print the list
-    def list_folders(self, container, root_folder):
-        folders = []
-        for folder in dbutils.fs.ls(f"/mnt/{container}/{root_folder}"):
-            folders.append(folder.name)
-        return folders
-        print(folders)
-        
+            blob_list = container_client.list_blobs(name_starts_with=prefix)
+            folders = set()
+
+            for blob in blob_list:
+                if "/" in blob.name:
+                    folder_name = blob.name.split("/")[0]
+                    folders.add(folder_name)
+
+            return list(folders)
+        except Exception as e:
+            print(f"Error listing folders in {container_name}: {str(e)}")
+            return []
+
+    def mount_containers(self):
+        for mount_point in self.mount_points:
+            try:
+                dbutils.fs.ls(mount_point)
+                print(f"{mount_point} is already mounted")
+            except Exception as e:
+                print(f"Mounting {mount_point}")
+                dbutils.fs.mount(
+                    source=f"wasbs://{mount_point}@{self.alpha_storage_account}",
+                    mount_point=mount_point,
+                    extra_configs={"<your_storage_account_config>": "<your_config_value>"}
+                )
+
+if __name__ == "__main__":
     datamigrator = DataMigrator()
-    datamigrator.mount_storage_containers()
-    alpha_folders = datamigrator.list_folders(datamigrator.alpha_container, "data")
-    
-    
+    datamigrator.mount_containers()
+
+    alpha_folders = datamigrator.list_folders(datamigrator.alpha_container, prefix="data")
+    beta_folders = datamigrator.list_folders(datamigrator.beta_container, prefix="data")
+
+    print("Alpha Folders:", alpha_folders)
+    print("Beta Folders:", beta_folders)
