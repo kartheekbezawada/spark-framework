7198069f179fda52066ef5844e3670fedb03e92b
kartheekbezawada <kartheekbezawada@outlook.com>
2023-12-27 23:42:11 +0000

    Update Migration_Blob_Folder_Info.py

--- Migration_Blob_Folder_Info.py
+++ Migration_Blob_Folder_Info.py
@@ -19,8 +19,16 @@ class BlobFolderInfo:
             blob_service_client = BlobServiceClient(account_url=self.blob_storage_url, credential=self.blob_account_key)
             container_client = blob_service_client.get_container_client(container_name)
             blob_list = container_client.list_blobs()
-            folder_list = [blob.name for blob in blob_list if blob.name.endswith('/')]
-            return folder_list
+
+            # Using a set to store unique folder names
+            folders = set()
+            for blob in blob_list:
+                # Extract directory names
+                directory_path = '/'.join(blob.name.split('/')[:-1])
+                if directory_path:  # If it's not an empty string, add to the set
+                    folders.add(directory_path)
+
+            return list(folders)
         except Exception as e:
             print(f"Error getting all folders in container {container_name}: {e}")
             raise e
@@ -29,6 +37,6 @@ if __name__ == "__main__":
     spark = SparkSession.builder.appName("Migration").getOrCreate()
     key_vault_scope = "key_vault_scope_migration"
     blob_folder_info = BlobFolderInfo(spark, key_vault_scope)
-    container_name = dbutils.secrets.get(scope=key_vault_scope, key="blob-container-name")
+    container_name = "container_name"
     folder_list = blob_folder_info.get_all_folders(container_name)
     print(folder_list)
