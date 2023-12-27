90f95dc7dbff3188cbb68b2e34a531e3f1ae4c8e
kartheekbezawada <kartheekbezawada@outlook.com>
2023-12-27 23:47:20 +0000

    Update Migration_Blob_Folder_Info.py

--- Migration_Blob_Folder_Info.py
+++ Migration_Blob_Folder_Info.py
@@ -33,10 +33,50 @@ class BlobFolderInfo:
             print(f"Error getting all folders in container {container_name}: {e}")
             raise e
 
+    def get_folders_size_in_mb(self, container_name):
+        try:
+            blob_service_client = BlobServiceClient(account_url=self.blob_storage_url, credential=self.blob_account_key)
+            container_client = blob_service_client.get_container_client(container_name)
+            blob_list = container_client.list_blobs()
+
+            # Dictionary to store folder sizes
+            folder_sizes = {}
+            for blob in blob_list:
+                # Extract directory names
+                directory_path = '/'.join(blob.name.split('/')[:-1])
+                if directory_path:
+                    # Accumulate sizes for each folder
+                    folder_sizes[directory_path] = folder_sizes.get(directory_path, 0) + blob.size
+
+            # Convert sizes to MB
+            for folder in folder_sizes:
+                folder_sizes[folder] = folder_sizes[folder] / (1024 * 1024)  # Convert from bytes to MB
+
+            return folder_sizes
+        except Exception as e:
+            print(f"Error getting sizes of folders in container {container_name}: {e}")
+            raise e
+        
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("Migration").getOrCreate()
     key_vault_scope = "key_vault_scope_migration"
+
+    # Initialize BlobFolderInfo instance
     blob_folder_info = BlobFolderInfo(spark, key_vault_scope)
-    container_name = "container_name"
+
+    # Retrieve the name of the container from key vault
+    container_name = dbutils.secrets.get(scope=key_vault_scope, key="blob-container-name")
+
+    # Get all folder names in the container
     folder_list = blob_folder_info.get_all_folders(container_name)
-    print(folder_list)
+    print("Folders in the container:")
+    for folder in folder_list:
+        print(folder)
+
+    # Get sizes of all folders in MB
+    folder_sizes_in_mb = blob_folder_info.get_folders_size_in_mb(container_name)
+    print("\nSizes of folders in MB:")
+    for folder, size in folder_sizes_in_mb.items():
+        print(f"{folder}: {size:.2f} MB")
+
+    spark.stop()
