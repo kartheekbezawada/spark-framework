1f64e496d3cca7ab6c87e955fcaffdd0aa50c062
kartheekbezawada <kartheekbezawada@outlook.com>
2024-02-16 18:58:49 +0000

    update

--- vaw.py
+++ vaw.py
@@ -9,8 +9,10 @@ class PayrollDataProcessor:
         self.alpha_account_name = "storage_account_name"
         self.alpha_account_key = "storage_account_key"
         self.alpha_container_name = "storage_container_name"
-        self.alpha_storage_config = {"fs.azure.account.key." + self.apha_account_name + ".blob.core.windows.net": self.apha_account_key}
-        self.alpha_storage_url = f"wasbs://{self.apha_container_name}@{self.apha_account_name}.blob.core.windows.net"
+        # Corrected variable names below
+        self.alpha_storage_config = {"fs.azure.account.key." + self.alpha_account_name + ".blob.core.windows.net": self.alpha_account_key}
+        self.alpha_storage_url = f"wasbs://{self.alpha_container_name}@{self.alpha_account_name}.blob.core.windows.net"
+
         
     def read_delta_table(self, path):
         table_path = f"{self.apha_storage_url}/{path}"
@@ -22,178 +24,181 @@ class PayrollDataProcessor:
         return df
 
     def process_colleague_rates(self, df):
-        window_spec = Window.partitionBy("colleague_id", "pay_code", "start_date").orderBy(col("start_date").desc())
-        processed_df = df.drop("md_process_id","md_source_ts","md_created_ts","md_source_path") \
-                         .withColumn("rate_seq", row_number().over(window_spec)) \
-                         .filter(col("start_date") <= current_date())
+        processed_df = df.drop("md_process_id","md_source_ts","md_created_ts","md_source_path").filter(col("start_date") <= current_date())
         return self.prefix_columns(processed_df, "cr")
 
     def process_colleague_base_rate(self, df):
-        window_spec = Window.partitionBy("colleague_id", "effective_date").orderBy(col("effective_date").desc())
-        processed_df = df.drop("md_process_id","md_source_ts","md_created_ts","md_source_path") \
-                         .withColumn("rate_seq", row_number().over(window_spec)) \
-                         .filter(col("effective_date") <= current_date())
+        processed_df = df.drop("md_process_id","md_source_ts","md_created_ts","md_source_path").filter(col("effective_date") <= current_date())               
         return self.prefix_columns(processed_df, "cbr")
-
-    def process_colleague_worked_hours(self, df):
-        processed_df = (df.drop("md_process_id", "md_source_ts", "md_created_ts", "md_source_path")
-                         .filter(col("datekey") == date_sub(current_date(), 4))
-                         .withColumn("datekey", col("work_start_time").cast("date"))
-                         .withColumn("store_number", substring(col("dock_name"), 1, 4))
-                         .withColumn("division_number", substring(col("dock_name"), 5, 8))
-                         .withColumn("year", year(col("work_start_time")))
-                         .withColumn("month", month(col("work_start_time")))
-                        )
-        return self.prefix_columns(processed_df, "cwh")
-
-    def process_wd_wb_mapping(self, df):
-        processed_df = df.drop("md_process_id","md_source_ts","md_created_ts","md_source_path")
-        return self.prefix_columns(processed_df, "wdwbmap")
 
     def process_div_cc_hierarchy(self, df):
         processed_df = df.drop("md_process_id","md_source_ts","md_created_ts","md_source_path")
         return self.prefix_columns(processed_df, "dcch")
-
-    def join_df(self, process_colleague_worked_hours, process_wd_wb_mapping, process_colleague_rates, process_colleague_base_rate):
-        joined_df = process_colleague_worked_hours.join(process_wd_wb_mapping,
-                        (col("cwh_tcode_name") == col("wdwbmap_tcode_name")) & 
-                        (col("cwh_htype_name") == col("wdwbmap_htype_name")) & 
-                        (col("cwh_emp_val4") == col("wdwbmap_emp_val4")), 
-                        "left_outer")
-
-        joined_df = joined_df.join(process_colleague_rates, 
-                       (col("cwh_pay_code") == col("cr_pay_code")) & 
-                       (col("cwh_colleague_id") == col("cr_colleague_id")) & 
-                       (col("cr_rate_seq") == 1), 
-                       "left_outer")
-
-        joined_df = joined_df.join(process_colleague_base_rate, 
-                       (col("cwh_colleague_id") == col("cbr_colleague_id")) & 
-                       (col("cbr_rate_seq") == 1), 
-                       "left_outer")
-
-        return joined_df
-
-
-    def select_and_rename_columns(self, df):
-        # Select and rename the required columns
-        return df.select(
-            "cwh_dock_name",
-            "cwh_emp_name",
-            "cwh_datekey",
-            "cwh_year",
-            "cwh_month",
-            "cwh_wrkd_starttime",
-            "cwh_wrkd_endtime",
-            "cwh_wrks_hrs",
-            "cwh_minor",
-            "cwh_seasonal",
-            "cwh_tcodename",
-            "cwh_htypename",
-            "cwh_htype_multiple",
-            "cwh_emp_val14",
-            "wdwbmap_paycode",
-            "wdwbmap_double_flag",
-            "cbr_basic_hourly_rate"
-        )
-
-    def add_composite_key(self, df):
-        df = df.withColumn("composite_key", 
-                             concat_ws("-", col("cwh_dock_name"), col("cwh_emp_name"), col("cwh_datekey"), col("cwh_htypename"), col("cwh_tcodename")))
-        return df
     
-    def apply_case_statement(self, df):
-        # Apply the case statement logic with multiple conditions
-        return df.withColumn(
-            "calculated_wages",
-            when(
-                (col("wdwbmap_paycode") == "R010") & (col("wdwbmap_double_flag").isNull()),
-                col("cbr_basic_hourly_rate").cast("decimal(6,2)") * 
-                col("cwh_wrkd_hrs").cast("decimal(6,2)") * 
-                col("cwh_htype_multiple").cast("decimal(6,2)")
-            ).when(
-                (col("wdwbmap_paycode") == "R010") & (col("wdwbmap_double_flag") == "y"),
-                2 * (col("cbr_basic_hourly_rate").cast("decimal(6,2)") *
-                col("cwh_wrkd_hrs").cast("decimal(6,2)"))
-            ).when(
-                (col("wdwbmap_paycode") != "R010") & (col("wdwbmap_double_flag").isNull()),
-                (col("cbr_basic_hourly_rate").cast("decimal(6,2)") + col("cr_value").cast("decimal(6,2)")) *
-                col("cwh_wrkd_hrs").cast("decimal(6,2)")
-            ).when(
-                (col("wdwbmap_paycode") != "R010") & (col("wdwbmap_double_flag") == "y"),
-                2 * (col("cbr_basic_hourly_rate").cast("decimal(6,2)") + col("cr_value").cast("decimal(6,2)")) *
-                col("cwh_wrkd_hrs").cast("decimal(6,2)")
-            ).otherwise(None)
-        )
+    def process_colleague_worked_hours(self, df):
+        processed_df = df.drop("md_process_id", "md_source_ts", "md_created_ts", "md_source_path")
+        return self.prefix_columns(processed_df, "cwh")
 
+    def process_wd_wb_mapping(self, processed_df):
+        return self.prefix_columns(processed_df, "wdwbmap")
+    
+    def process_calendar(self, df):
+        processed_df = df.drop("md_process_id","md_source_ts","md_created_ts","md_source_path")
+        return prosessed_df
 
-    def process_and_transform_data(self, rates_path, base_rate_path, worked_hours_path, mapping_path):
-        # Reading data from Delta Lake tables
-        colleague_rates_df = self.read_delta_table(rates_path)
-        colleague_base_rate_df = self.read_delta_table(base_rate_path)
-        colleague_worked_hours_df = self.read_delta_table(worked_hours_path)
-        wd_wb_mapping_df = self.read_delta_table(mapping_path)
-
-        # Processing data
-        pcr = self.process_colleague_rates(colleague_rates_df)
-        pcbr = self.process_colleague_base_rate(colleague_base_rate_df)
-        pcwhrs = self.process_colleague_worked_hours(colleague_worked_hours_df)
-        pwdwbm = self.process_wd_wb_mapping(wd_wb_mapping_df)
+    def transform_colleague_worked_hours(self, df):
+        cwh.createOrReplaceTempView("cwh")
+        cd.createOrReplaceTempView("cd")
+        cd1.createOrReplaceTempView("cd1")
         
-        # Joining DataFrames
-        joined_df = self.join_df(pcwhrs, pwdwbm, pcr, pcbr)
-        # Transforming DataFrames
-        df_with_columns = self.select_and_rename_columns(joined_df)
-        composite_key_df = self.add_composite_key(df_with_columns)
-        transformed_df = self.apply_case_statement(composite_key_df)
-       
-
-        return transformed_df
-    
+        query = """
+        SELECT
+            cwh.cwh_dock_name,
+            substring(cast(cwh.cwh_dock_name as string), 1, 4) as store_number, 
+            substring(cast(cwh.cwh_dock_name as string), 5, 8) as division,
+            cwh_emp_name as colleague_id,
+            cwh.cwh_wrkd_hrs,
+            cwh.cwh_wrks_workdate as datekey,
+            cwh.cwh_wrkd_Start_time,
+            cwh.cwh_wrkd_end_time,
+            cwh.cwh_tcode_name,
+            cwh.cwh_htype_name,
+            cwh.cwh_emp_val4,
+            from 
+            cwh
+            join cd
+            on cwh.cwh_wrks_work_date = cd.calendar_date
+            where exists (
+                select * from cd1 
+                where cd1.wm_week = cd.wm_week
+                and cd1.calendar_year = cd.calendar_year
+                and cd1.calendar_date = current_date() -1 day
                 
-    def write_delta_table(self, df, delta_table_path):
-        # Define the full path for the Delta table, including the storage configuration
-        full_delta_table_path = f"{self.alpha_storage_url}/{delta_table_path}"
-    
-        # Check if the Delta table exists at the specified path
-        if DeltaTable.isDeltaTable(self.spark, full_delta_table_path):
-            deltaTable = DeltaTable.forPath(self.spark, full_delta_table_path)
+            )
+        """
+        result_df = self.spark.sql(query)
+        return result_df
+            
+    def create_temp_views(cwh_processed, wd_wb_map_processed, cbr_processed, cr_processed, div_cc_processed, cr_processed_a1, cbr_processed_b1):
+        cwh_processed.createOrReplaceTempView("cwh_processed")
+        wd_wb_map_processed.createOrReplaceTempView("wd_wb_mapping_processed")
+        cbr_processed.createOrReplaceTempView("cbr_processed")
+        cr_processed.createOrReplaceTempView("cr_processed")
+        div_cc_processed.createOrReplaceTempView("div_cc_processed")
+        cr_processed_a1.createOrReplaceTempView("cr_processed_a1")
+        cbr_processed_b1.createOrReplaceTempView("cbr_processed_b1")
+
+        def transform_colleague_rates(session):
+            create_temp_views(session.cwh_processed, session.wd_wb_map_processed, session.cbr_processed,
+                      session.cr_processed, session.div_cc_processed, session.cr_processed_a1,
+                      session.cbr_processed_b1)
+
+        query = """
+        SELECT
+            k.date_key,
+            k.store_number,
+            dcc.vaw_division,
+            COALESCE(k.wrk_hrs, 0.00) as Actual_Hours,
+            COALESCE(SUM(k.calculated_wages), 0.00) as Actual_Wages
+        FROM
+            (SELECT
+                c.cwh_wrks_work_date as date_key,
+                c.cwh_store_number as store_number,
+                c.cwh_division,
+                c.cwh_tcode_name,
+                c.cwh_htype_name,
+                c.cwh_emp_val4,
+                d.wdwbmap_pay_code as mp_pay_code,
+                c.cwh_emp_name as wb_cid,
+                a.cr_colleague_id as wd_bcid,
+                b.cbr_cost_centre,
+                c.cwh_wrkd_hrs,
+                a.cr_pay_code as wd_pay_code,
+                a.cr_pay_unit,
+                CASE
+                    WHEN d.wdwbmap_pay_code <> 'R010' THEN CAST(b.cbr_basic_hourly_rate AS numeric(6,2)) + CAST(a.cr_value AS numeric(6,2)) END AS premium_rate,
+                CASE
+                    WHEN d.wdwbmap_pay_code = 'R010' THEN b.cbr_basic_hourly_rate END AS basic_rate,
+                CASE
+                    WHEN (d.wdwbmap_pay_code = 'R010' AND d.wdwbmap_double_flag IS NULL) THEN CAST(b.cbr_basic_hourly_rate AS numeric(6,2)) * CAST(c.cwh_wrkd_hrs AS numeric(6,2))
+                    WHEN (d.wdwbmap_pay_code = 'R010' AND d.wdwbmap_double_flag = 'Y') THEN 2 * CAST(b.cbr_basic_hourly_rate AS numeric(6,2)) * CAST(c.cwh_wrkd_hrs AS numeric(6,2))
+                    WHEN (d.wdwbmap_pay_code <> 'R010' AND d.wdwbmap_double_flag IS NULL) THEN (CAST(b.cbr_basic_hourly_rate AS numeric(6,2)) + CAST(a.cr_value AS numeric(6,2))) * CAST(c.cwh_wrkd_hrs AS numeric(6,2))
+                    WHEN (d.wdwbmap_pay_code <> 'R010' AND d.wdwbmap_double_flag = 'Y') THEN 2 * (CAST(b.cbr_basic_hourly_rate AS numeric(6,2)) + CAST(a.cr_value AS numeric(6,2))) * CAST(c.cwh_wrkd_hrs AS numeric(6,2))
+                END AS calculated_wages,
+                d.wdwbmap_double_flag,
+                b.cbr_effective_date,
+                b.cbr_job_code,
+                c.cwh_wrkd_Start_time,
+                c.cwh_wrkd_end_time
+            FROM
+                cwh_processed AS c
+                LEFT OUTER JOIN wd_wb_map_processed AS d ON c.cwh_tcode_name = d.wdwbmap_tcode_name
+                AND c.cwh_htype_name = d.wdwbmap_htype_name
+                AND c.cwh_emp_val4 = d.wdwbmap_emp_val4
+                LEFT OUTER JOIN cr_processed AS a ON d.wdwbmap_pay_code = a.cr_pay_code
+                AND c.cwh_emp_name = a.cr_colleague_id
+                AND a.cr_start_date <= c.cwh_wrks_work_date
+                AND a.cr_start_date = (SELECT MAX(cr_start_date) FROM cr_processed_a1 
+                                       WHERE a1.cr_colleague_id = a.cr_colleague_id
+                                       AND a1.cr_pay_code = a.cr_pay_code
+                                       AND a1.cr_start_date <= c.cwh_wrks_work_date)
+                LEFT OUTER JOIN cbr_processed AS b ON b.cbr_colleague_id = c.cwh_emp_name
+                AND b.cbr_effective_date <= c.cwh_wrks_work_date
+                AND b.cbr_effective_date = (SELECT MAX(cbr_effective_date) FROM cbr_processed_b1
+                                            WHERE b1.cbr_colleague_id = b.cbr_colleague_id
+                                            AND b1.cbr_effective_date <= c.cwh_wrks_work_date)) k
+            LEFT OUTER JOIN div_cc_processed AS dcc ON k.cwh_division = dcc.dcch_cc_mapping
+        GROUP BY
+            k.date_key, k.store_number, dcc.vaw_division
+        ORDER BY k.date_key, k.store_number, dcc.vaw_division
+        """
         
-            deltaTable.alias("target").merge(
-                df.alias("source"),
-                "target.composite_key = source.composite_key"
-            ).whenMatchedUpdateAll() \
-            .whenNotMatchedInsertAll() \
-         .execute()
-        else:
-            df.write.format("delta") \
-                .mode("append") \
-                .option("overwriteSchema", "true") \
-                .saveAsTable(full_delta_table_path)
-        
-if __name__ == "__main__":
-    spark = SparkSession.builder.appName("PayrollDataProcessorApp").getOrCreate()
-                        
-    processor = PayrollDataProcessor(spark)
+        result_df = self.spark.sql(query)
+        return result_df
     
-    # Access alpha_account_name and alpha_account_key from the processor instance
-    alpha_account_name = processor.alpha_account_name
-    alpha_account_key = processor.alpha_account_key
+    if __name__ == "__main__":
+    
+        spark = SparkSession.builder.appName("PayrollDataProcessor").getOrCreate()
+        processor = PayrollDataProcessor(spark)
+    
+        colleague_rates_path = "colleague_rates"
+        cr =  processor.read_delta_table(colleague_rates_path)
+        cr_processed = processor.process_colleague_rates(cr)
+    
+        colleague_rates_path = "colleague_rates"
+        cr1 =  processor.read_delta_table(colleague_rates_path)
+        cr_processed_a1 = processor.process_colleague_rates(cr1)
 
-    # If you need to use them for further Spark session configuration, do it here
-    # For example, if you need to reconfigure the Spark session with these details (though typically you'd set these at SparkSession creation)
-    spark.conf.set(f"fs.azure.account.key.{alpha_account_name}.blob.core.windows.net", alpha_account_key)
-
-    # Proceed with processing
-    transformed_df = processor.process_and_transform_data(
-        "delta_lake_path/wd_colleague_rates",
-        "delta_lake_path/wd_colleague_base_rate",
-        "delta_lake_path/wb_colleague_hours",
-        "delta_lake_path/vaw_wd_wb_mapping"
-    )
-
-    # Example of showing the transformed DataFrame
-    transformed_df.show()
-    # Example of writing the transformed DataFrame
-    delta_table_path = "path/to/delta_table"
-    processor.write_delta_table(transformed_df, delta_table_path)
+        colleague_base_rate_path = "colleague_base_rate"
+        cbr =  processor.read_delta_table(colleague_base_rate_path)
+        cbr_processed = processor.process_colleague_base_rate(cbr)
+    
+        colleague_base_rate_path = "colleague_base_rate"
+        cbr1 =  processor.read_delta_table(colleague_base_rate_path)
+        cbr_processed_b1 = processor.process_colleague_base_rate(cbr1)
+    
+        div_cc_hierarchy_path = "div_cc_hierarchy"
+        div_cc =  processor.read_delta_table(div_cc_hierarchy_path)
+        div_cc_processed = processor.process_div_cc_hierarchy(div_cc)
+    
+        colleague_worked_hours_path = "colleague_worked_hours"
+        cwh =  processor.read_delta_table(colleague_worked_hours_path)
+        cwh_processed = processor.process_colleague_worked_hours(cwh)
+    
+        wd_wb_mapping_path = "wd_wb_mapping"
+        wd_wb_map =  processor.read_delta_table(wd_wb_mapping_path)
+        wd_wb_map_processed = processor.process_wd_wb_mapping(wd_wb_map)
+    
+        dim_calendar_path = "dim_calendar"
+        dim_cc =  processor.read_delta_table(dim_calendar_path)
+        dim_cc_processed = processor.process_calendar(dim_cc)
+    
+        result_df = processor.transform_colleague_worked_hours(cwh_processed, wd_wb_map_processed, cbr_processed, cr_processed, div_cc_processed, cr_processed_a1, cbr_processed_b1)
+        result_df.show()
+    
+    
+    
+    
+    
+            
+       
