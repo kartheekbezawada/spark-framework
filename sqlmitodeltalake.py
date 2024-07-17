b30fab50074f6af444a69f72ce820f53b95adcd3
kartheekbezawada <kartheekbezawada@outlook.com>
2024-07-17 10:02:37 +0100

    Update sqlmitodeltalake.py

--- sqlmitodeltalake.py
+++ sqlmitodeltalake.py
@@ -5,6 +5,7 @@ from pyspark.sql.functions import col, max
 from delta.tables import DeltaTable
 from pyspark.sql.utils import AnalysisException
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, DateType, TimestampType, BooleanType, LongType, DecimalType, ArrayType
+from concurrent.futures import ThreadPoolExecutor
 
 class DataMigration:
 
@@ -49,7 +50,7 @@ class DataMigration:
     # Get max value of a column from Delta Lake table
     def get_max_value(self, table_name, column_name):
         try:
-            last_max_value = self.spark.read.format("delta").table(table_name).select(max(column_name)).collect()[0]["last_max_value"]
+            last_max_value = self.spark.read.format("delta").table(table_name).select(max(column_name)).collect()[0][0]
         except AnalysisException:
             last_max_value = None
         return last_max_value
@@ -70,7 +71,9 @@ class DataMigration:
     # Read data in partitions from Azure SQL MI table
     def read_full_data_in_partitions(self, schema_name, table_name, partition_column, num_partitions):
         dbtable = f"{schema_name}.{table_name}"
-        query = f"(SELECT MIN({partition_column}) AS min_value, MAX({partition_column}) AS max_value FROM {dbtable}) AS bounds"
+    
+        # Query to get distinct partition values
+        query = f"(SELECT DISTINCT {partition_column} FROM {dbtable}) as query"
         bounds_df = self.spark.read \
             .format("jdbc") \
             .option("url", self.jdbc_url) \
@@ -79,24 +82,41 @@ class DataMigration:
             .option("password", self.jdbc_password) \
             .option("driver", self.jdbc_driver) \
             .load()
+    
+        # Extract distinct partition values
+        partition_values = [row[partition_column] for row in bounds_df.collect()]
+        partition_values.sort()
+    
+        # Initialize list to store DataFrames
+        partition_dfs = []
+    
+        # Function to fetch data for a partition
+        def fetch_partition_data(partition_value):
+            partition_query = f"(SELECT * FROM {dbtable} WHERE {partition_column} = '{partition_value}') as query"
+            partition_df = self.spark.read \
+                .format("jdbc") \
+                .option("url", self.jdbc_url) \
+                .option("dbtable", partition_query) \
+                .option("user", self.jdbc_username) \
+                .option("password", self.jdbc_password) \
+                .option("driver", self.jdbc_driver) \
+                .load()
+            return (partition_value, partition_df)
+    
+        # Use ThreadPoolExecutor to process partitions in parallel
+        with ThreadPoolExecutor(max_workers=num_partitions) as executor:
+            futures = [executor.submit(fetch_partition_data, pv) for pv in partition_values]
+            for future in futures:
+                partition_dfs.append(future.result())
+    
+        return partition_dfs
 
-        bounds = bounds_df.collect()[0]
-        lower_bound = bounds["min_value"]
-        upper_bound = bounds["max_value"]
+    # Function to write partition data
+    def write_partition(self, partition):
+        partition_value, partition_df = partition
+        delta_path = f"{self.delta_storage_url}/partition={partition_value}"
+        partition_df.write.format("delta").mode("overwrite").save(delta_path)
 
-        return self.spark.read \
-            .format("jdbc") \
-            .option("url", self.jdbc_url) \
-            .option("dbtable", dbtable) \
-            .option("user", self.jdbc_username) \
-            .option("password", self.jdbc_password) \
-            .option("driver", self.jdbc_driver) \
-            .option("partitionColumn", partition_column) \
-            .option("lowerBound", lower_bound) \
-            .option("upperBound", upper_bound) \
-            .option("numPartitions", num_partitions) \
-            .load()
-        
     # Write data to Delta Lake with overwrite mode
     def write_overwrite(self, df, delta_path, partition_column=None):
         write_mode = "overwrite"
@@ -107,15 +127,15 @@ class DataMigration:
                 df.write.format("delta").mode(write_mode).save(delta_path)
         except AnalysisException as e:
             print(f"Error writing data to {delta_path}: {e}")
-                
+
     # Write overwrite mode by partition overwrite mode dynamic as option
-    def write_overwrite_by_partition(self, df, delta_path, partition_column=None):
+    def write_overwrite_by_partition(self, partition_dfs, delta_path):
         write_mode = "overwrite"
         try:
-            if partition_column:
-                df.write.format("delta").mode(write_mode).partitionBy(partition_column).option("partitionOverwriteMode", "dynamic").save(delta_path)
-            else:
-                df.write.format("delta").mode(write_mode).save(delta_path)
+            with ThreadPoolExecutor(max_workers=len(partition_dfs)) as executor:
+                futures = [executor.submit(self.write_partition, partition) for partition in partition_dfs]
+                for future in futures:
+                    future.result()
         except AnalysisException as e:
             print(f"Error writing data to {delta_path}: {e}")
     
@@ -129,11 +149,11 @@ class DataMigration:
     def process_incremental_table(self, schema_name, table_name, delta_path, partition_column=None, incremental_column=None):
         last_max_value = self.get_max_value(delta_path, incremental_column)
         if last_max_value is None:
-            df = self.read_full_data_in_partitions(schema_name, table_name, partition_column, num_partitions=700)  # Adjust num_partitions as needed
-            df.foreachPartition(lambda partition: self.write_partition(partition, delta_path, partition_column))
+            partition_dfs = self.read_full_data_in_partitions(schema_name, table_name, partition_column, num_partitions=700)  # Adjust num_partitions as needed
+            self.write_overwrite_by_partition(partition_dfs, delta_path)
         else:
             df = self.read_incremental_data_from_sqlmi(schema_name, table_name, incremental_column, last_max_value)
-            self.write_overwrite_by_partition(df, delta_path, partition_column)
+            self.write_overwrite_by_partition([(None, df)], delta_path)
         print(f"Incremental data written to {delta_path}")
 
 if __name__ == "__main__":
